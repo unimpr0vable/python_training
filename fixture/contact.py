88449191ca4e1324050ad8b295d6ac9b2987a73f
gurovaae <albina.gurova@akbarsdigital.ru>
2022-05-07 14:40:18 +0300

    Fix:add waiter in logout, optimize contact\group with fill_form,add open_contact_page

--- fixture/contact.py
+++ fixture/contact.py
@@ -12,33 +12,7 @@ class ContactHelper:
     def create(self, contact):
         wd = self.app.wd
         self.open_page_add_new()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("theform").click()
+        self.fill_contact_form(contact)
         # submit contact creation
         wd.find_element_by_name("submit").click()
         self.return_to_home_page()
@@ -47,8 +21,13 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
 
+    def open_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+
     def delete_first(self):
         wd = self.app.wd
+        self.open_contact_page()
         #select first contact
         wd.find_element_by_name("selected[]").click()
         #submit deletion
@@ -57,8 +36,15 @@ class ContactHelper:
 
     def edit_first(self, contact):
         wd = self.app.wd
+        self.open_contact_page()
         wd.find_element_by_xpath("//img[@title='Edit']").click()
-        # fill contact form
+        self.fill_contact_form(contact)
+        # submit contact update
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
@@ -79,7 +65,4 @@ class ContactHelper:
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys(contact.email)
-        # submit contact update
-        wd.find_element_by_name("update").click()
-        self.return_to_home_page()
 
