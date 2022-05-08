ca876fa371d0d8bec552338316e0d1a8f919576e
gurovaae <albina.gurova@akbarsdigital.ru>
2022-05-08 10:33:19 +0300

    Use if-else in edit_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -28,41 +28,41 @@ class ContactHelper:
     def delete_first(self):
         wd = self.app.wd
         self.open_contact_page()
-        #select first contact
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         #submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
 
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
     def edit_first(self, contact):
         wd = self.app.wd
         self.open_contact_page()
-        wd.find_element_by_xpath("//img[@title='Edit']").click()
+        self.select_first_contact_for_edit()
         self.fill_contact_form(contact)
         # submit contact update
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
 
+    def select_first_contact_for_edit(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@title='Edit']").click()
+
     def fill_contact_form(self, contact):
         wd = self.app.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
+        self.change_field("firstname", contact.firstname)
+        self.change_field("lastname", contact.lastname)
+        self.change_field("nickname", contact.nickname)
+        self.change_field("company", contact.company)
+        self.change_field("mobile", contact.mobile)
+        self.change_field("email", contact.email)
+
+    def change_field(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,7 @@
 
 class Contact:
 
-    def __init__(self, firstname, lastname, nickname, company, mobile, email):
+    def __init__(self, firstname=None, lastname=None, nickname=None, company=None, mobile=None, email=None):
         self.firstname = firstname
         self.lastname = lastname
         self.nickname = nickname
