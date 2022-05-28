d5448dfa91e76a6c4e264e47ae894b06c035a664
gurovaae <albina.gurova@akbarsdigital.ru>
2022-05-28 22:04:01 +0300

    Add parameters from command line: browser and url

--- conftest.py
+++ conftest.py
@@ -7,11 +7,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url = base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url = base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -21,3 +23,8 @@ def stop(request):
         fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(finish)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
